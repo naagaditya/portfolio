752944305814b13df04639fd6c35c20ab413ab79
adityanaag <adityanaag91@gmail.com>
2018-11-27 17:20:00 +0530

    push notification

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  'messagingSenderId': '720246550114'
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notification = JSON.parse(payload.data.notification);
+  const notificationOptions = {
+    body: notification.body,
+    icon: notification.icon
+  };
+
+  return self.registration.showNotification(notification.title,
+    notificationOptions);
+});
